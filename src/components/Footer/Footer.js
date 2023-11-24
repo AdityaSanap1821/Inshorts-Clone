2eafdc5d56bdb0841b75b06f7a9f13e2fa0a035e
AdityaSanap1821 <151859441+AdityaSanap1821@users.noreply.github.com>
2023-11-24 16:51:53 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span className="name">
+
+
+
+      </span>
+      <hr style={{ width: "90%" }} />
+      <div className="iconContainer">
+
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
